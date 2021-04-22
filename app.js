d24d974a3dd4f9a807b6b562c5c9d1ef24c56fc4
Toni Halmetoja <36439969+ToniHalmetoja@users.noreply.github.com>
2021-04-22 13:49:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-var corsOptions = {
-    allowedHeaders: 'Content-Type',
-  }
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,7 +12,7 @@ var app = express();
 
 const MongoClient = require("mongodb").MongoClient; 
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
